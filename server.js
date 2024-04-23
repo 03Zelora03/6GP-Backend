09a0abaaf12c40c6fca8328a99833804c5accc75
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-23 19:24:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('/modifyobject', async (req, res) => {
 
 app.post('/sendvideo', upload.single('video'), async (req, res) =>{
   console.log('incoming request')
+  res.send("It works!")
 })
 
 app.get('/hello', (req, res) => {
