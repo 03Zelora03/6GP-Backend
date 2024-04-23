1931d3d6308624f7c8404b32346c033af3c74fcf
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-22 20:07:57 -0400

    Update database.js

--- database.js
+++ database.js
@@ -8,15 +8,19 @@ const pool = mariadb.createPool({
 });
 
 async function testDatabase(){
-    pool.getConnection()
-    .then(conn => {
-        conn.query("SELECT 1 as val")
-        .then((rows) => {
-            console.log("hello")
-            return rows
-        })
-    })
-    return null
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT 1 as val");
+        console.log(rows); //[ {val: 1}, meta: ... ]
+        //const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 }
 
 module.exports = {
