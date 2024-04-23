5513bce49dc95fbb385508999678fb39a100018f
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-22 20:08:58 -0400

    Update database.js

--- database.js
+++ database.js
@@ -13,6 +13,7 @@ async function testDatabase(){
         conn = await pool.getConnection();
         const rows = await conn.query("SELECT 1 as val");
         console.log(rows); //[ {val: 1}, meta: ... ]
+        return rows
         //const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
         //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 
