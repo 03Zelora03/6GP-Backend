2ef24f83bafc421ca750b95337d082f5b3e39339
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-22 20:03:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/test', async (req, res) => {
-  const rows = await db.testDatabase()
-  console.log("Rows: ", rows)
-  res.send(rows)
+  db.testDatabase().then(rows => {
+    console.log("Rows: ", rows)
+    res.send(rows)
+  })
 })
 
 app.get('/hello', (req, res) => {
