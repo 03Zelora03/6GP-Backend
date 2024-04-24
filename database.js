9d5b32078328f45325f29daf4d57cb1cea41a9e8
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-23 21:06:37 -0400

    Update database.js

--- database.js
+++ database.js
@@ -57,7 +57,13 @@ async function addVideo(nom, taille, md5, dirnom, objet){
         conn = await pool.getConnection();
         const res = await conn.query(`SELECT ordre FROM video_objets ORDER BY ordre DESC`);
         console.log(res)
-        //const nextOrder = res[0].ordre + 1
+        let nextOrder = 0
+        if(res[0].ordre){
+            nextOrder = res[0].ordre + 1
+        }  
+
+        //REST OF CODE HERE (i think you just need to uncomment)
+
         //const realRes = await conn.query(`INSERT INTO video_objets (nom, taille, md5, ordre, dirnom, objet) VALUES ('${nom}', ${taille}, '${md5}', ${nextOrder}, '${dirnom}', ${objet})`)
         //return realRes
         return ":3"
