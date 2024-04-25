0883741876764f514fc4519365a5a7f8f2180665
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-25 17:43:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express()
 const port = 50000
 
 app.use(cors())
+app.use(express.urlencoded({extended: false})).use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
