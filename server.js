c49e7d79f8a0ba654751aa4414233f3f24a4346d
03Zelora03 <106628890+03Zelora03@users.noreply.github.com>
2024-04-26 10:00:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/updatevideo', async (req, res) => {
   res.redirect('back')
 })
 
-app.get('download/:filename', async (req, res) => {
+app.get('/download/:filename', async (req, res) => {
   const filepath = `/home/admis/6GP-Backend/videos/${req.params.filename}.mp4`
   res.sendFile(filepath)
 })
